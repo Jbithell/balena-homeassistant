2ee36a8f0fc8df241ed2ecc243390405acb1ea43
James Bithell <james@bithell.com>
2021-04-08 09:55:14 +0100

    Remove balena sense elements

--- homeassistant/start.sh
+++ homeassistant/start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-cp /tmp/sense.yaml /config/sense.yaml
-cp /tmp/sensors.sh /config/sensors.sh
-cp /tmp/lovelace.sh /config/lovelace.sh
+#cp /tmp/lovelace.sh /config/lovelace.sh
 
-chmod +x /config/sensors.sh
-chmod +x /config/lovelace.sh
-
-if [ ! -f /config/ui-lovelace.yaml ]; then cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml; fi 
+#chmod +x /config/sensors.sh
 
 python -m homeassistant --config /config
